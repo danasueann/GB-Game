281678ddc913b878c6aae1b908e17cfbb832aec6
danasueann <dana2407dg@gmail.com>
2025-06-12 20:54:26 +0200

    Thats a simple dev environment that I can use later

--- main.c
+++ main.c
@@ -1,21 +1,9 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "Test.c"
+
 
 
 void main()
 {
-    // Initialize the Game
-    
-    printf("TEST3\n");
-    set_bkg_data(0, 1, Test); // Clear background data
-
-
-    //Fill the background with the Test data
-    init_bkg(0);
-
-    //Show the background
-    SHOW_BKG;
-    DISPLAY_ON;
 
 }
