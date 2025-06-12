f32384e97a3b4555ed7f45c4ed05e984ed9398d6
danasueann <dana2407dg@gmail.com>
2025-06-12 20:17:36 +0200

    Setup for debugger and emulator

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "Test.c"
+
 
 void main()
 {
     // Initialize the Game
-    printf("Hello, Game");
+    
+    printf("TEST\n");
+    set_bkg_data(0, 1, Test); // Clear background data
+
+
+    //Fill the background with the Test data
+    init_bkg(0);
+
+    //Show the background
+    SHOW_BKG;
+    DISPLAY_ON;
+
 }
